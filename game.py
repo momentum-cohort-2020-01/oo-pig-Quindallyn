6dab1e0e05b8400771a6566ccae9a0fad26b83dd
Quindallyn <qrk5002@gmail.com>
2020-02-20 09:34:50 -0500

    attempt to translate words instructions into code

---
+++ game.py
@@ -0,0 +1,61 @@
+from random import sample
+
+VALUE = [1,2,3,4,5,6]
+
+class Game:
+    def __init__(self):
+        self.player = Player(Quin)
+        self.dealer = Dealer(Brandon)
+        self.score = 
+     
+    def who_goes_first(self)
+        if random method returns 1-6 Player goes first
+        else Dealer goes first 
+        pass
+
+
+class Player:
+    def __init__(self, name):
+    self.name = name
+
+    def __str__(self, name):
+        return f'{self.name}'
+    
+    def something(something):
+        if rolls_die == >1 + round_score 
+            if rolls_dies == 1 
+            return 
+
+        pass
+
+class Dealer:
+    def __init__(self):
+    
+    def __str__(self, name):
+        return f'{self.name}'
+        
+def something(something):
+        if rolls_die == >1 
+        return + round_score 
+            if rolls_dies == 1 
+            return total_d_score == 0 and change turns
+                if round_score < 20 
+                return dealer rolls_die
+                if round_score >= 20 
+                return 
+pass
+
+
+class Dice:
+    def __init__(self, sides):
+        self.sides = sides
+
+    def __str__(self):
+        return f'(self.sides)'
+
+    def roll_die():
+
+    pass
+
+game = Game()
+    str()
